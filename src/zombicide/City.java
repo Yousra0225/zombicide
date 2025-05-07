62475bec326aaa322090d6ec716b412b8b1f6414
Yousra0225 <chbibyousra02@gmail.com>
2025-05-07 15:40:25 +0200

    feat(city): add attributs and the constructor of the class

--- src/zombicide/City.java
+++ src/zombicide/City.java
@@ -3,4 +3,14 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class City {};
+public abstract class City {
+    protected int width;
+    protected int height; 
+
+    // Constructor
+    public City(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+}
