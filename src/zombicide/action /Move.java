7fa619947590552fb8336e02b639d89f0737ee3b
Yousra0225 <chbibyousra02@gmail.com>
2025-05-20 15:19:29 +0200

    feat(action): add execute method

--- src/zombicide/action /Move.java
+++ src/zombicide/action /Move.java
@@ -1,8 +1,13 @@
 package zombicide.action ;
 
 public class Move implements Action {
+    @Override
     public String getName(){
         return "Move" ;
     }
-    
+
+    @Override
+    public void execute() {
+        System.out.println("Executing move action...");
+    }
 }
