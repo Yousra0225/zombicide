203a4871cc326bf5d25d6a17d9939715c3a95857
Yousra0225 <chbibyousra02@gmail.com>
2025-05-20 15:17:46 +0200

    feat(action): add getName method

--- src/zombicide/action /Action.java
+++ src/zombicide/action /Action.java
@@ -1,5 +1,10 @@
 package zombicide.action ;
 
 public class Action {
-    
+    public String getName() {
+        return "Action";
+    }
+    public void execute() {
+        System.out.println("Action executed");
+    }
 }
